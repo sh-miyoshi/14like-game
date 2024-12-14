67699b2b1838a302c1ecf2d47b03a847ed697029
s.miyoshi <s.miyoshi1991@gmail.com>
2024-12-14 15:08:49 +0900

    add hp

--- pkg/app/models/object/enemy.go
+++ pkg/app/models/object/enemy.go
@@ -7,17 +7,21 @@ import (
 )
 
 const (
+	EnemyHPSize    = 200
 	Enemy1HitRange = 150
 )
 
 type Enemy1 struct {
-	pos point.Point
-	// hp  int
+	pos   point.Point
+	hp    int
+	hpMax int
 }
 
 func (e *Enemy1) Init() {
 	e.pos.X = config.ScreenSizeX * 3 / 4
 	e.pos.Y = config.ScreenSizeY / 2
+	e.hpMax = 1000
+	e.hp = e.hpMax
 }
 
 func (e *Enemy1) End() {
@@ -25,6 +29,12 @@ func (e *Enemy1) End() {
 
 func (e *Enemy1) Draw() {
 	dxlib.DrawCircle(e.pos.X, e.pos.Y, Enemy1HitRange, dxlib.GetColor(255, 255, 255), false)
+
+	tx := config.ScreenSizeX - EnemyHPSize - 40
+	ty := 30
+	dxlib.DrawBox(tx, ty, tx+EnemyHPSize, ty+20, dxlib.GetColor(255, 255, 255), false)
+	size := EnemyHPSize * e.hp / e.hpMax
+	dxlib.DrawBox(tx, ty, tx+size, ty+20, dxlib.GetColor(255, 255, 255), true)
 }
 
 func (e *Enemy1) Update() {
